141e3be088cedf9ebdded3a4be60eb714e6a46ca
Rafael Cosman <rcosman@stanford.edu>
2014-03-05 17:23:45 -0800

    Added BlockGrid object (container for a grid of blocks) and Ship object which extends it

---
+++ gameClasses/Ship.js
@@ -0,0 +1,16 @@
+var Block = IgeEntity.extend({
+	classId: 'Block',
+	
+	init: function () {
+		IgeEntity.prototype.init.call(this);
+
+		if (!ige.isServer) {
+			this.texture(ige.client.textures.block_power_gold)
+					.width(100)
+					.height(100);
+
+		}
+	}
+});
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = Block; }
