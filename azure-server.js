9c6f654957b2ec4de30011547a4b01f2e638c7a4
Derrick Liu <dliu@lediur.com>
2014-03-11 23:29:50 -0700

    Merge of 11 commits:
    Enable Azure debugging information
    
    Add node process command line in attempt to start IGE server
    
    Revert "Add node process command line in attempt to start IGE server"
    
    This reverts commit 66c3cb2297d0ac3cff43bd067b2eeb330c4aff76.
    
    Adding Node engine specs to package.json
    
    Add main property to package.json
    
    Add Web.config to redirect requests to IISnode at IGE
    
    Revert "Add Web.config to redirect requests to IISnode at IGE"
    
    This reverts commit c08855c36c627385fc120e220df71e5a87c233d9.
    
    Add dependencies to package.json
    
    Adding full Web.config again
    
    Adding separate azure-server js to launch Azure server (hacky)
    
    Revert "Adding full Web.config again"
    
    This reverts commit 9914bb1a9c6961e6168b5c53742f34d57420ee67.
    
    Forgot to change package.json to reflect changes

---
+++ azure-server.js
@@ -0,0 +1,49 @@
+console.log('Azure - launching Cosmos server on node.js v' + process.version);
+
+// Set a global variable for the location of
+// the node_modules folder
+modulePath = 'node_modules';
+
+// Load the CoreConfig.js file
+igeCoreConfig = require('ige/engine/CoreConfig.js');
+
+var arr = igeCoreConfig.include,
+  arrCount = arr.length,
+  arrIndex,
+  arrItem,
+  itemJs;
+
+// Check if we are deploying, if so don't include core modules
+var argParse = require("node-arguments").process,
+  args = argParse(process.argv, {separator:'-'});
+
+if (!args['-deploy']) {
+  // Loop the igeCoreConfig object's include array
+  // and load the required files
+  for (arrIndex = 0; arrIndex < arrCount; arrIndex++) {
+    arrItem = arr[arrIndex];
+    if (arrItem[0].indexOf('s') > -1) {
+      itemJs = arrItem[1] + ' = ' + 'require("ige/engine/' + arrItem[2] + '")';
+      // Check if there is a specific object we want to use from the
+      // module we are loading
+      if (arrItem[3]) {
+        itemJs += '.' + arrItem[3] + ';';
+      } else {
+        itemJs += ';';
+      }
+      eval(itemJs);
+    }
+  }
+} else {
+  // Just include the basics to run IgeNode
+  IgeBase = require('ige/engine/core/IgeBase');
+  IgeClass = require('ige/engine/core/IgeClass');
+}
+
+// Include the control class
+IgeNode = require('ige/server/IgeNode');
+
+// Start the app - with Cosmos
+process.argv.push('-g');
+process.argv.push('.');
+var igeNode = new IgeNode();
