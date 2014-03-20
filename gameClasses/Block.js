7a852fbc275669ad9a1035f3f5dc743ff169ca6b
Rafael Cosman <rcosman@stanford.edu>
2014-03-20 16:44:20 -0700

    added hp to blocks

--- gameClasses/Block.js
+++ gameClasses/Block.js
@@ -6,7 +6,17 @@ var Block = IgeEntity.extend({
 
 		this.width(100).height(100);
 
+		this.hp = 10; //this is the default hp of all blocks. Subclasses of block can have a different hp.
+
 		if (!ige.isServer) {
+		}
+	}
+
+	damage: function(amount) {
+		this.hp -= amount;
+
+		if (this.hp <= 0) {
+			//TODO break the block from its block grid if it is in a block grid
 		}
 	}
 });
