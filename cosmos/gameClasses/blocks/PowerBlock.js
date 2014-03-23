612a8854a4c45445cd48b07266c792c01f53af39
Daniel Chiu <daniechiu@gmail.com>
2014-03-23 23:58:17 +0100

    Load SVG XML data from static server before game starts.

--- cosmos/gameClasses/blocks/PowerBlock.js
+++ cosmos/gameClasses/blocks/PowerBlock.js
@@ -8,7 +8,6 @@ var PowerBlock = Block.extend({
 			this.textureBackground = "rgb(242, 242, 242)";
 			this.textureOutline = "rgb(255, 190, 13)";
 			this.textureSvg = ige.client.svgs.power;
-			this.textureSvgUrl = "./assets/blocks/power/power.svg";
 		}
 	}
 });
