4629cd0ba7198b2808034bb4271d192589b437f2
Rafael Cosman <rcosman@stanford.edu>
2014-03-27 08:56:16 +0100

    Added random asteroid generator in AsteroidGenerator.js
    NOT YET TESTED

--- cosmos/gameClasses/AsteroidGenerator.js
+++ cosmos/gameClasses/AsteroidGenerator.js
@@ -5,14 +5,42 @@ var AsteroidGenerator = {
 	littleAsteroid: function () {
 		return [
 			[new Carbon(), new Iron()],
-			[new Iron(), new Carbon()]
+			[new Iron(),   new Carbon()]
 		]
 	},
 
 	/**
-	* A random asteroid
+	 * A hollow asteroid
+	 */
+	hollowAsteroid: function() {
+		return [
+			[new Carbon(), new Carbon(), new Iron()],
+			[new Iron(),   undefined,    new Carbon()],
+			[new Iron(),   new Iron(),   new Iron()]
+		]
+	}
+
+	/**
+	* A random asteroid which may have holes in it
 	*/
 	randomAsteroid: function () {
+		asteroid = []
+
+		var maxSize = 10;
+		var probabilityOfHole = .2;
+
+		for(var x = 0; x < Math.random() * maxSize) {
+			rowList = [];
+			for(var y = 0; y < Math.random() * maxSize) {
+				if(Math.random() < probabilityOfHole) {
+					rowList.push(undefined);
+				}
+				else {
+					rowList.push(new Iron());
+				}
+			}
+			asteroid.push(rowList);
+		}
 	}
 };
 
