7c3d80addd3e9fb9ec62f84be09c3b9bd04c2c21
Rafael Cosman <rcosman@stanford.edu>
2014-03-27 10:34:38 +0100

    commented GameInit.js

--- cosmos/gameClasses/GameInit.js
+++ cosmos/gameClasses/GameInit.js
@@ -20,7 +20,7 @@ var GameInit = {
 			.id('vp1')
 			.autoSize(true)
 			.scene(game.mainScene)
-			.drawBounds(false)
+			.drawBounds(false) //draws the axis aligned bounding boxes. Set to true for debugging.
 			.mount(ige);
 
 		this.initScenes(game);
