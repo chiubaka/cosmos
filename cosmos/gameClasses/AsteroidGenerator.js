edfdf876c7a397f5eec3211ae333b1fe9d3a110c
Derrick Liu <dliu@lediur.com>
2014-04-02 17:37:09 -0700

    Fix formatting in old asteroid generation function

--- cosmos/gameClasses/AsteroidGenerator.js
+++ cosmos/gameClasses/AsteroidGenerator.js
@@ -8,16 +8,15 @@ var AsteroidGenerator = {
 		var maxSize = maxSize || 40;
 		var probabilityOfHole = probabilityOfHole || .2;
 
-		for(var x = 0; x < Math.random() * maxSize; x++) {
+		for (var x = 0; x < Math.random() * maxSize; x++) {
 			rowList = [];
-			for(var y = 0; y < Math.random() * maxSize; y++) {
-				if(Math.random() < probabilityOfHole) {
+			for (var y = 0; y < Math.random() * maxSize; y++) {
+				if (Math.random() < probabilityOfHole) {
 					rowList.push(undefined);
-				}
-				else {
-					if(Math.random() < .3) {
+				} else {
+					if (Math.random() < .3) {
 						rowList.push(new IronBlock());
-					} else if(Math.random() < .3) {
+					} else if (Math.random() < .3) {
 						rowList.push(new CarbonBlock());
 					} else {
 						rowList.push(new IceBlock());
