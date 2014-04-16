fb3ec492730eef9bc30654d74a0f14e1374ce058
Eric Thong <ethong@stanford.edu>
2014-04-15 23:21:20 -0700

    Change fixture debug flag back to false

--- cosmos/gameClasses/ServerNetworkEvents.js
+++ cosmos/gameClasses/ServerNetworkEvents.js
@@ -33,7 +33,7 @@ var ServerNetworkEvents = {
 	_onPlayerEntity: function(data, clientId) {
 		if (!ige.server.players[clientId]) {
 			ige.server.players[clientId] = new Player(clientId)
-				.debugFixtures(true)//call this before calling setGrid()
+				.debugFixtures(false)//call this before calling setGrid()
 				.grid(ExampleShips.starterShipSingleMisplacedEngine())
 				.addSensor(300)
 				.attractionStrength(1)
