f28c30b12c6b88d6fe2fe76d8edf31c18e63da40
Derrick Liu <dliu@lediur.com>
2014-04-22 16:58:18 -0700

    Add stubs for cargo models

---
+++ cosmos/gameClasses/CargoContainer.js
@@ -0,0 +1,14 @@
+﻿var CargoContainer = IgeClass.extend({
+	classId: 'CargoContainer',
+
+	_block: undefined,
+	_items: [],
+
+	init: function() {
+		throw "Not implemented!";
+	},
+	
+	getItems: function(itemId) {
+		throw "Not implemented";
+	}
+});
---
+++ cosmos/gameClasses/CargoItem.js
@@ -0,0 +1,29 @@
+﻿var CargoItem = IgeClass.extend({
+    classId: 'CargoItem',
+    
+	/**
+	 * Whether or not this CargoItem is a chunk placeholder in the player's cargo inventory that contains
+	 * a certain number of chunks.
+	 * 
+	 * The limit of the number of chunks is set by the Cargo model.
+	 */
+    _isChunkBlock: false,
+    
+	/**
+	 * If this CargoItem is a chunk block, contains the chunks that are contained in the placeholder.
+	 * 
+	 * All chunks are moved as a single entity.
+	 */
+    _chunks: [],
+    
+	/**
+	 * If this CargoItem represents a whole block, this represents the block that is contained within.
+	 * 
+	 * May require refactoring of the Block model or adding a new model to represent a block so it doesn't contain world-specific info.
+	 */
+    _entity: undefined,
+
+    init: function() {
+	    throw "Not implemented!";
+    }
+});
