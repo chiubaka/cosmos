1694a6f7eff34e2efe6fb36bf96f3dea52d81b57
Derrick Liu <dliu@lediur.com>
2014-04-25 02:33:26 -0700

    Remember to return the UUID generated...

--- cosmos/lib/UuidGenerator.js
+++ cosmos/lib/UuidGenerator.js
@@ -3,7 +3,7 @@
 	 * Generates a UUID according to RFC4122.
 	 */
 	gen: function() {
-		'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
 			return v.toString(16);
 		});
