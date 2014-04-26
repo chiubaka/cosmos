e3b0fb23b08118751497dc7b6f6a1a9aa8710ce5
Derrick Liu <dliu@lediur.com>
2014-04-25 17:11:44 -0700

    Refine and complete implementations of Cargo interfaces

--- cosmos/models/cargo/Cargo.js
+++ cosmos/models/cargo/Cargo.js
@@ -1,4 +1,4 @@
-﻿var Cargo = IgeClass.extend({
+var Cargo = IgeClass.extend({
 	classId: 'Cargo',
 	
 	/**
@@ -42,15 +42,29 @@
 
 		return this._isUnlimited;
 	},
-	
-	//
-	// ITEM MANAGEMENT
-	//
 
-	addItem: function(item) {
+	addBlock: function(block) {
+		if (block === undefined) {
+			return this;
+		}
+
+		// Construct a new item from the block
+		var cargoItem = new CargoItem(block);
+		return this.addCargoItem(cargoItem);
+	},
+
+	addBlocks: function(blocks) {
+		for (var block in blocks) {
+			this.addBlock(block);
+		}
+
+		return this;
+	},
+
+	addCargoItem: function(item) {
 		if (item === undefined || !this.hasSpace()) {
 			// Do nothing, abort.
-			return this;
+			return false;
 		}
 		
 		if (this.getNumContainers() == 0) {
@@ -60,62 +74,93 @@
 			if (this.unlimitedSpace()) {
 				// Create a new cargo container to contain the item.
 				this._containers.push(new CargoContainer());
+			} else {
+				// Abort, we can't add anything.
+				return false;
 			}
 		}
 		
 		// Add items to the item list and to an available cargo container.
+		this._items.push(item);
+
 		// Right now, linearly fill up the containers.
 		for (var i = 0; i < this.getNumContainers(); i++) {
 			// Check if the container has space.
 			var container = this.getContainer(i);
 			if (container.hasSpace()) {
-				container.addItem(item);
+				return container.linkItem(item);
 			} else {
 				continue;
 			}
 		}
 
-		return this;
+		// Should never get here
+		return false;
 	},
 	
-	addItems: function(items) {
+	addCargoItems: function(items) {
 		for (var item in items) {
-			this.addItem(item);
+			this.addCargoItem(item);
 		}
 
 		return this;
 	},
 
 	removeItem: function(itemId) {
-		for (var i = 0; i < this.getNumContainers(); i++) {
-			var container = this.getContainer(i);
-			if (container.hasItem(itemId)) {
-				container.removeItem(itemId);
-				return this;
-			}
-		}
+		var itemToRemove = this.getItem(itemId);
 
-		return this;
+		// Remove the item from the item list
+		var itemIndex = this._items.indexOf(itemToRemove);
+		ArrayUtils.remove(this._items, itemIndex);
+
+		// Remove the item from a container.
+		var itemContainer = itemToRemove.container();
+		itemContainer.unlinkItem(itemId);
 	},
 
 	removeItems: function(itemType) {
-		for (var i = 0; i < this.getNumContainers(); i++) {
-			var container = this.getContainer(i);
-			if (container.hasItemType(itemType)) {
-				container.removeItems(itemType);
-				return this;
+		// Find all of the items in the list to remove
+		var indicesForRemoval = [];
+		for (var i = 0; i < this._items.length; i++) {
+			var item = this._items[i];
+			if (item.type() === itemType) {
+				indicesForRemoval.push(i);
 			}
+		}
+
+		// Now, remove all selected elements and unlink them via removeItem().
+		for (var index in indicesForRemoval) {
+			var item = this._items[index];
+			this.removeItem(item.uuid());
 		}
 
 		return this;
 	},
-	
-	getItems: function(itemType) {
-		throw "Not implemented!";
+
+	getItem: function(itemId) {
+		for (var item in this._items) {
+			if (item.uuid() === itemId) {
+				return item;
+			}
+		}
+
+		return undefined;
 	},
 	
-	listItems: function() {
-		throw "Not implemented!";
+	getItems: function(itemType) {
+		var results = [];
+		for (var item in this._items) {
+			if (item.type() === itemType) {
+				results.push(item);
+			}
+		}
+	},
+	
+	listItems: function(clone) {
+		if (clone !== undefined && clone) {
+			return JSON.parse(JSON.stringify(clone));
+		}
+		return this._items;
 	},
 	
 	hasSpace: function() {
@@ -138,8 +183,15 @@
 	/**
 	 * Check if the player's cargo inventory contains a particular type of item.
 	 */
-	hasItem: function(itemType) {
-		throw "Not implemented!";
+	hasItemType: function(itemType) {
+		for (var i = 0; i < this.getNumContainers() ; i++) {
+			var container = this.getContainer(i);
+			if (container.hasItemType(itemType)) {
+				return true;
+			}
+		}
+
+		return false;
 	},
 	
 	/** 
