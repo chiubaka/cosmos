0037320ca03d1f41a08b7c3f0d272bdc2228674d
Derrick Liu <dliu@lediur.com>
2014-04-25 17:16:08 -0700

    Refine and complete implementation of CargoContainer interfaces

--- cosmos/models/cargo/CargoContainer.js
+++ cosmos/models/cargo/CargoContainer.js
@@ -1,40 +1,51 @@
-﻿var CargoContainer = IgeClass.extend({
+var CargoContainer = IgeClass.extend({
 	classId: 'CargoContainer',
 
-	_block: undefined,
 	_itemStore: undefined,
 	_capacity: 1,
 
 	init: function(capacity) {
-		this._itemStore = {};
+		this._itemStore = [];
 		this._capacity = capacity;
 	},
-	
-	addItem: function(item) {
+
+	linkItem: function(item) {
 		if (!this.hasSpace()) {
 			throw "No more space in this cargo container!";
 		}
 
-		this._itemStore[uuid] = item;
+		this._itemStore.push(item);
 		return item.uuid();
 	},
 
+	unlinkItem: function(itemId) {
+		throw "Not implemented";
+	},
+
 	hasItem: function(itemId) {
-		return this._itemStore.hasOwnProperty(itemId);
+		for (var item in this._itemStore) {
+			if (item.uuid() === itemId) {
+				return true;
+			}
+		}
 	},
-	
-	getItems: function(itemId) {
-		return this._itemStore[itemId];
+
+	getItem: function(itemId) {
+		for (var item in this._itemStore) {
+			if (item.uuid() === itemId) {
+				return item;
+			}
+		}
 	},
-	
+
 	capacity: function() {
 		return this._capacity;
 	},
-	
+
 	numItems: function() {
 		return this._itemStore.length;
 	},
-	
+
 	hasSpace: function() {
 		return ((this.capacity() - this.numItems()) > 0);
 	}
