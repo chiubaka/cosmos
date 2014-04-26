c600a6e64fb0d6faa8ec0136d7eb08a12cf02a4e
Derrick Liu <dliu@lediur.com>
2014-04-25 17:48:35 -0700

    Wrote preliminary documentation for CargoItem.js

--- cosmos/models/cargo/CargoItem.js
+++ cosmos/models/cargo/CargoItem.js
@@ -19,6 +19,13 @@ var CargoItem = IgeClass.extend({
 
 	_lastModified: undefined,
 
+	/**
+	 * Initializes this CargoItem with a provided block. 
+	 * 
+	 * Generates a UUID for this CargoItem object and initializes the last modified timestamp.
+	 * 
+	 * @param block the block this item should represent
+	 */
     init: function(block) {
     	this._id = UuidGenerator.gen();
     	this._entity = JSON.parse(JSON.stringify(block));
@@ -26,10 +33,21 @@ var CargoItem = IgeClass.extend({
 	    this.updateLastModified();
     },
 
+	/**
+	 * Updates the time that this CargoItem object was last modified.
+	 */
 	updateLastModified: function() {
 		this._lastModified = Date.now();
 	},
 
+	/**
+	 * Gets or sets the container that this CargoItem is associated with.
+	 * 
+	 * Lets us associate CargoItems with specific CargoContainers, which will prove useful later
+	 * when grouping items in certain physical cargo blocks.
+	 * 
+	 * @param container the CargoContainer that this item is associated with
+	 */
 	container: function(container) {
 		if (container !== undefined) {
 			this._container = container;
@@ -40,6 +58,9 @@ var CargoItem = IgeClass.extend({
 		return this._container;
 	},
 
+	/**
+	 * Gets the type of the entity represented by this CargoItem
+	 */
 	type: function() {
 		return this._entity.classId;
 	},
