dbfe5ff561f6ecbc199cf41baa8e98ffd84bf953
Eric Thong <ethong@stanford.edu>
2014-04-27 02:05:05 -0700

    Fix formatting, add comment

--- cosmos/gameClasses/mining/LaserBeam.js
+++ cosmos/gameClasses/mining/LaserBeam.js
@@ -7,12 +7,9 @@ var LaserBeam = IgeEntity.extend({
 		// Correct for texture rotation
 		this.rotateTo(0, 0, Math.radians(90))
 
+		// No need for server to be concerned with tweening
 		if (!ige.isServer) {
-			this.texture(ige.client.textures.laserBeamTexture)
-
-				//.rotateTo(0, 0, Math.radians(90))
 				.width(300)
-					// No need for server to be concerned with animation
 				.newTween();
 		}
 	},
@@ -25,7 +22,7 @@ var LaserBeam = IgeEntity.extend({
 		this._rotate.tween()
 			.stepTo({z: restAngle + sweepAngle/2}, 1000, 'inOutCubic')
 			.stepTo({z: restAngle - sweepAngle/2}, 1000, 'inOutCubic')
-			//.stepTo({z: Math.radians(90)}, 500, 'outQuad')
+			// From the 1st step, repeat forever
 			.repeatMode (1, -1)
 			.start();
 
