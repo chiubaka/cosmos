cf842398399bb75975ba85b729e1f560af55c357
Derrick Liu <dliu@lediur.com>
2014-04-27 23:18:13 -0700

    Substantially refactor CargoContainer.js to align with Eric's parameters that will be passed into event handlers

--- cosmos/models/cargo/CargoContainer.js
+++ cosmos/models/cargo/CargoContainer.js
@@ -2,102 +2,149 @@
  * CargoContainer.js
  * @author Derrick Liu
  * 
- * The CargoContainer class represents a container owned by the player that holds some number
- * of CargoItems.
+ * The CargoContainer class represents a container owned by the player that holds some blocks.
  * 
- * CargoContainers are used to enable others to extend functionality so individual cargo blocks
- * or groups of blocks on a player's ship can be arbitrarily defined and grouped as cargo
- * containers in the future.
+ * The base CargoContainer is intended to provide a common interface for other CargoInterfaces
+ * to build upon in a way that extends the player's cargo inventory functionality.
  */
 var CargoContainer = IgeClass.extend({
 	classId: 'CargoContainer',
 
+	STACK_SIZE: 20,
+
 	_itemStore: undefined,
-	_capacity: 1,
+	_numSlots: 1,
 
 	/**
 	 * Initialize a cargo container.
 	 */
-	init: function(capacity) {
-		this._itemStore = [];
-		this._capacity = capacity;
+	init: function(numSlots) {
+		this._itemStore = {};
+
+		if (numSlots !== undefined) {
+			this._numSlots = numSlots;
+		}
 	},
 
 	/**
-	 * Links an item in the player's cargo inventory with this
-	 * cargo container (i.e. "store" this cargo container the
-	 * item)
+	 * Add a particular CargoItem to this container.
 	 */
-	linkItem: function(item) {
-		if (!this.hasSpace()) {
-			throw "No more space in this cargo container!";
+	addItem: function(item) {
+		if (!this.hasSpace(item)) {
+			return false;
 		}
 
-		this._itemStore.push(item);
-		return item.uuid();
+		if (!this._itemStore.hasOwnProperty(item.type())) {
+			this._itemStore[item.type()] = 0;
+		}
+
+		this._itemStore[item.type()]++;
+		return true;
 	},
 
 	/**
-	 * Unlinks an item in the player's cargo inventory from this cargo
-	 * container (i.e. the item is "removed" from this cargo container)
+	 * Remove a number of items with a specific type from the container.
+	 * @param itemType the types of items to remove (given by classId)
+	 * @param quantity the number of items to remove (upper bound)
 	 */
-	unlinkItem: function(itemId) {
-		var indexToRemove = -1;
-		for (var i = 0; i < this._itemStore.length; i++) {
-			if (this._itemStore[i].uuid() === itemId) {
-				indexToRemove = i;
-				break;
-			}
+	removeType: function(itemType, quantity) {
+		if (!this._itemStore.hasOwnProperty(itemType)) {
+			return 0;
 		}
 
-		if (indexToRemove >= 0) {
-			this._itemStore.remove(indexToRemove);
+		var numToRemove = Math.min(this._itemStore[itemType], quantity);
+		this._itemStore[itemType] -= numToRemove;
+		if (this._itemStore[itemType] <= 0) {
+			delete this._itemStore[itemType];
 		}
+
+		return numToRemove;
 	},
 
 	/**
-	 * Whether or not the cargo container "contains" an item with a
-	 * particular itemId
+	 * Extracts a specific number of items from the container.
+	 * This is similar to a withdrawal: the items will be removed from the container
+	 * and returned to the caller.
+	 * 
+	 * @param itemType the item's type
+	 * @param quantity the number of items to extract
 	 */
-	hasItem: function(itemId) {
-		for (var item in this._itemStore) {
-			if (item.uuid() === itemId) {
-				return true;
-			}
+	extractItem: function(itemType, quantity) {
+		var extracted = [];
+
+		if (!this._itemStore.hasOwnProperty(itemType)) {
+			return extracted;
 		}
+
+		var numToExtract = Math.min(this._itemStore[itemType], quantity);
+		this._itemStore[itemType] -= numToExtract;
+		if (this._itemStore[itemType] <= 0) {
+			delete this._itemStore[itemType];
+		}
+
+		for (var i = 0; i < numToExtract; i++) {
+			extracted.push(Block.blockFromClassId(itemType));
+		}
+
+		return extracted;
 	},
 
 	/**
-	 * Get an item with a particular UUID that is contained in this
-	 * cargo container.
+	 * Whether or not more elements can be added to the cargo container
 	 */
-	getItem: function(itemId) {
-		for (var item in this._itemStore) {
-			if (item.uuid() === itemId) {
-				return item;
-			}
+	hasSpace: function(item) {
+		var itemType = item.type();
+		if (this._itemStore.hasOwnProperty(itemType)) {
+			// We might be able to stuff this into an existing slot.
+			return (this._itemStore[itemType] < this.STACK_SIZE);
+		} else {
+			// We'll need to allocate a new slot for this.
+			var numItems = Object.keys(this._itemStore).length;
+			return (numItems < this.numSlots());
 		}
 	},
 
 	/**
-	 * Gets the capacity of the cargo container.
+	 * Gets the number of slots in the cargo container.
+	 */
+	numSlots: function() {
+		return this._numSlots;
+	},
+
+	/**
+	 * Gets the full capacity of the cargo container (num slots * num items per slot)
 	 */
 	capacity: function() {
-		return this._capacity;
+		return this._numSlots * this.STACK_SIZE;
 	},
 
 	/**
 	 * Gets the number of CargoItems contained in this cargo container
 	 */
 	numItems: function() {
-		return this._itemStore.length;
+		var sum = 0;
+		for(var itemType in this._itemStore) {
+			if (this._itemStore.hasOwnProperty(itemType)) {
+				sum += this._itemStore[itemType];
+			}
+		}
+		return sum;
 	},
 
-	/**
-	 * Whether or not more elements can be added to the cargo container
-	 */
-	hasSpace: function() {
-		return ((this.capacity() - this.numItems()) > 0);
+	numOfType: function(itemType) {
+		if (this._itemStore.hasOwnProperty(itemType)) {
+			return this._itemStore[itemType];
+		}
+
+		return 0;
+	},
+
+	debugDump: function() {
+		console.log(":: numSlots " + this.numSlots());
+		console.log(":: numItems " + this.numItems());
+		for (var key in this._itemStore) {
+			console.log(":: block type: " + key + ", #[" + this._itemStore[key] + "]");
+		}
 	}
 });
 
