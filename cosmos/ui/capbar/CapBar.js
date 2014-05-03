9ee7a23e97385e0bd66bbd20172a74a0a710ede4
Derrick Liu <dliu@lediur.com>
2014-05-03 01:56:40 -0700

    Mount capabilities to Capbar and arrange at the bottom of the screen appropriately

--- cosmos/ui/capbar/CapBar.js
+++ cosmos/ui/capbar/CapBar.js
@@ -22,5 +22,28 @@
 			'bottom': 0,
 			'height': this.HEIGHT
 		});
+
+		this._caps = [];
+		this._caps.push(new MiningCap());
+		this._caps.push(new ConstructCap());
+
+		this.mountCaps();
 	},
+
+	mountCaps: function() {
+		var capbarWidth = this.width();
+		var capbarCenter = capbarWidth / 2;
+		var numCaps = this._caps.length;
+
+		for (var i = 0; i < this._caps.length; i++) {
+			var cap = this._caps[i];
+
+			var capOriginOffset = (cap.width() / 2);
+			var capFromCenterOffset = ((cap.width() + this.CAP_SPACING) * ((numCaps - 1) / 2 - i));
+			var xPos = capbarCenter - capOriginOffset - capFromCenterOffset;
+
+			cap.translateTo(xPos, cap.translate().y(), 0);
+			cap.mount(this);
+		}
+	}
 });
