5e99873404d71436a97ff7c278da81223689aeef
Derrick Liu <dliu@lediur.com>
2014-05-06 15:04:53 -0700

    Change Capbar styles to be more readable

--- cosmos/ui/capbar/CapBar.js
+++ cosmos/ui/capbar/CapBar.js
@@ -15,9 +15,11 @@
 		IgeUiElement.prototype.init.call(this);
 
 		this.applyStyle({
-			'backgroundColor': '#dddddd',
+			'backgroundColor': 'rgba(30,30,30,1)',
 			'borderColor': 'rgba(0,0,0,0)',
 			'bottom': 0,
+			'left': 0,
+			'right': 0,
 			'height': this.HEIGHT
 		});
 
