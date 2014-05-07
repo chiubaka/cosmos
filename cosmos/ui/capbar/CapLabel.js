02aaafd85e40a03135c638b98b7946198b63a3b7
Derrick Liu <dliu@lediur.com>
2014-05-07 00:46:27 -0700

    Change CapLabel styling to avoid ID conflicts with other labels spawned from Tools or Caps with the same classId

--- cosmos/ui/capbar/CapLabel.js
+++ cosmos/ui/capbar/CapLabel.js
@@ -6,19 +6,19 @@
 	init: function(id, value, color) {
 		IgeUiLabel.prototype.init.call(this);
 
-		var styleId = id + '-label';
-		ige.ui.style('#' + styleId, {
+		var styleClass = id + '-label';
+		ige.ui.style('.' + styleClass, {
 			'color': 'rgba(0, 0, 0, 0)',
 		});
 
-		ige.ui.style('#' + styleId + ':hover', {
+		ige.ui.style('.' + styleClass + ':hover', {
 			'color': color
 		});
 
 		this.font("12pt Segoe UI Semibold")
 			.padding(0)
 			.value(value)
-			.id(styleId);					 
+			.styleClass(styleClass);					 
 
 		// Need to manually calculate width because labels don't do it automatically
 		// Increase by 10px to offset margin compensation
@@ -26,6 +26,6 @@
 		this.width(calcWidth);
 
 		// Move labels above icons, translate to the right to make 
-		this.translateTo(5, -this.height() - this.LABEL_MARGIN, 0);
+		this.translateTo(5, Math.floor(-this.height() - this.LABEL_MARGIN), 0);
 	},
 });
