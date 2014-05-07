088999781951389a09b87dc3f7d2d17e27fa936b
Derrick Liu <dliu@lediur.com>
2014-05-07 02:19:18 -0700

    Implement block mounting and drawing on the construction toolbar

--- cosmos/ui/toolbar/tools/CargoTool.js
+++ cosmos/ui/toolbar/tools/CargoTool.js
@@ -1,3 +1,26 @@
 ﻿var CargoTool = Tool.extend({
 	classId: 'CargoTool',
+
+	// Since we are drawing a Block as an icon, we don't want to draw a picture.
+	// Therefore, blank out the stub and active icons so no picture is drawn.
+	STUB_ICON: '',
+	ACTIVE_ICON: '',
+
+	BG_COLOR: 'rgb(3,131,255)',
+
+	mount: function(elem) {
+		Tool.prototype.mount.call(this, elem);
+
+		this.mountBlock();
+	},
+
+	mountBlock: function() {
+		// Get the appropriate block from the block type
+		this._mountedBlock = Block.prototype.blockFromClassId(this.TOOL_NAME);
+		this._mountedBlock.mount(this);
+
+		if (this._quantity !== undefined) {
+			this._mountedBlock.translateBy(0, -this._quantity.width() / 2, 0);
+		}
+	}
 });
