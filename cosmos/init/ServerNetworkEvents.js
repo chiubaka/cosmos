19731816e52f706a6ee35d9c25a81a531eb1c6d5
Eric Thong <ethong@stanford.edu>
2014-05-07 12:53:11 -0700

    Whitespace fix

--- cosmos/init/ServerNetworkEvents.js
+++ cosmos/init/ServerNetworkEvents.js
@@ -56,7 +56,6 @@ var ServerNetworkEvents = {
 	// TODO: User access control. Restrict what players can do based on clientId
 	// TODO: Guard against undefined blocks (do not trust client) so server doesn't crash
 	_onBlockClicked: function(data, clientId) {
-
 		var player = ige.server.players[clientId];
 
 		// Do not start mining if we are already mining
