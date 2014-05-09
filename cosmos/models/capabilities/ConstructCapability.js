ac8942e288b1d407e4ab085b509c296aab5538f0
Derrick Liu <dliu@lediur.com>
2014-05-08 17:49:29 -0700

    Instrument capabilities

--- cosmos/models/capabilities/ConstructCapability.js
+++ cosmos/models/capabilities/ConstructCapability.js
@@ -57,6 +57,7 @@
 	 */
 	ClickScene_mouseDown: function(sender, event, data) {
 		data.selectedType = this.capability.selectedType;
+		ige.client.metrics.fireEvent('construct', 'new', this.capability.selectedType);
 		ige.network.send('constructNew', data);
 	}
 });
