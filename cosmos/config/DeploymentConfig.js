03e1e7b3367e372d2cc6e78627c6062f18daf2f2
Derrick Liu <dliu@lediur.com>
2014-05-10 03:47:28 +0000

    Add preview client and server URLs to DeploymentConfig

--- cosmos/config/DeploymentConfig.js
+++ cosmos/config/DeploymentConfig.js
@@ -6,6 +6,8 @@
 	CLIENTURL_DEV: "dev.cosmos.teamleonine.com",
 	CLIENTURL_STAGING: "stg.cosmos.teamleonine.com",
 	CLIENTURL_RELEASE: "cosmos.teamleonine.com",
+	CLIENTURL_PREVIEW: "preview.cosmos.teamleonine.com",
+	CLIENTURL_PREVIEW_AZURE: "tl-cosmos-client-preview.azurewebsites.net",
 
 	/**
 	 * Deployed server URLs
@@ -14,6 +16,7 @@
 	SERVERURL_DEV: "http://dev.srv.cosmos.teamleonine.com",
 	SERVERURL_STAGING: "http://stg.srv.cosmos.teamleonine.com",
 	SERVERURL_RELEASE: "http://srv.cosmos.teamleonine.com",
+	SERVERURL_PREVIEW: "http://preview.srv.teamleonine.com"
 };
 
 if (typeof (module) !== 'undefined' && typeof (module.exports) !== 'undefined') {
