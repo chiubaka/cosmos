c6cc1e582f56c5c02f717547a0135a8844f804bf
Eric Thong <ethong@stanford.edu>
2014-05-23 02:04:57 -0700

    Remove dead code. Use update IGE mongodb component

--- cosmos/db/DbConfig.js
+++ cosmos/db/DbConfig.js
@@ -10,39 +10,6 @@ var DbConfig = {
 		collection: 'players'
 	},
 
-	init: function() {
-		var module_path = '../../node_modules/mongodb'
-		this.Db = require(module_path).Db;
-		this.Server = require(module_path).Server;
-		this.MongoClient = require(module_path).MongoClient;
-		this.Collection = require(module_path).Collection;
-	},
-
-	connect: function (callback) {
-		var self = this;
-
-		// Build mongoDB URI string
-		var format = require('util').format;
-		var username = encodeURIComponent(this.config.username);
-		var password = encodeURIComponent(this.config.password);
-		var formatString = 'mongodb://%s:%s@%s:%s/%s'
-		var uri = format(formatString, username, password, this.config.host,
-			this.config.port, this.config.dbName);
-
-		// Connect to MongoDB server
-		var opts = {uri_decode_auth: true, native_parser: true};
-		this.MongoClient.connect(uri, opts, function(err, db) {
-			// TODO: Use IGE component and logging system
-			assert.equal(err, null, "DB connect error");
-			self.db = db;
-			callback()
-		});
-	},
-
-	disconnect: function () {
-		this.db.close();
-	}
-
 };
 
 if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = DbConfig; }
