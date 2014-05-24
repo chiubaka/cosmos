2fedb5f28a43b1265f60caa0650dbd383b671c58
Rafael Cosman <rcosman@stanford.edu>
2014-05-23 22:09:38 -0700

    added regular ship blocks to the derelict spaceships.
    This branch is ready to merge.

--- cosmos/entities/scenery/AsteroidGenerator.js
+++ cosmos/entities/scenery/AsteroidGenerator.js
@@ -248,10 +248,11 @@ var AsteroidGenerator = {
 		},
 
 		SHIP_PARTS: {
-			"EngineBlock": .25,
-			"FuelBlock": .25,
-			"PowerBlock": .25,
-			"ThrusterBlock": .25
+			"EngineBlock": .1,
+			"FuelBlock": .2,
+			"PowerBlock": .2,
+			"ThrusterBlock": .1,
+			"Block": .4
 		}
 	}
 };
