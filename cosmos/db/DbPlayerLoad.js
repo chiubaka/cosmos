a804cf43f3c7c414659ba5cbf86be3f977462ccc
Eric Thong <ethong@stanford.edu>
2014-05-25 15:09:04 -0700

    Clean up player load

--- cosmos/db/DbPlayerLoad.js
+++ cosmos/db/DbPlayerLoad.js
@@ -2,22 +2,12 @@ var DbPlayerLoad = {
 
 	load: function(authToken, callback) {
 		var self = this;
-		// TODO: Remove excess callbacks, use the correct find function,
-
 		ige.mongo.db.collection('players', function(err, players) {
 			players.count(function(err, count) {
-				console.log(count);
 				// Get a random space ship from the db
 				var randomNum = Math.floor((Math.random() * count));
-				players.find({},{'limit':-1, 'skip':randomNum}, function (err, cursor) {
-					cursor.toArray(function(err, docs) {
-						var x = docs;
-						// TODO: Find out why doc and err works when reversed
-						docs.each(function(doc, err) {
-							console.log(doc);
-							callback.apply(self, [err, doc.ship]);
-						});
-					});
+				players.find({},{'limit':-1, 'skip':randomNum}).toArray(function(err, docs) {
+					callback(err, docs[0].ship);
 				});
 			});
 		});
