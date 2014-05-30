45f23671ac655abbb4481fb7357641fc402acd2d
Derrick Liu <dliu@lediur.com>
2014-05-30 14:28:55 -0700

    Small style tweaks

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,20 +1,20 @@
 function signInTransitionIn() {
     // Run the exitContent animation and then the enterContent animation
     // Use the recommended offset by leaving the offset argument empty to get the best performance
-    WinJS.UI.Animation.continuumForwardOut($("#login-container").get(0), null).done(function () {
+    WinJS.UI.Animation.exitPage($("#login-container").get(0), null).done(function () {
         $("#login-intro").css('display', 'none');
         $("#login-providers").css('display', 'block');
-        return WinJS.UI.Animation.continuumForwardIn($("#login-container").get(0), null);
+        return WinJS.UI.Animation.enterPage($("#login-container").get(0), null);
     });
 }
 
 function signInGoBack() {
     // Run the exitContent animation and then the enterContent animation
     // Use the recommended offset by leaving the offset argument empty to get the best performance
-    WinJS.UI.Animation.continuumBackwardOut($("#login-container").get(0), null).done(function () {
+    WinJS.UI.Animation.exitPage($("#login-container").get(0), null).done(function () {
         $("#login-providers").css('display', 'none');
         $("#login-intro").css('display', 'block');
-        return WinJS.UI.Animation.continuumBackwardIn($("#login-container").get(0), null);
+        return WinJS.UI.Animation.enterPage($("#login-container").get(0), null);
     });
 }
 
@@ -46,4 +46,9 @@ $(document).ready(function() {
     $("#login-providers .screen-back").first().click(function() {
         signInGoBack();
     });
+
+    WinJS.UI.Animation.enterPage([
+        $('#cosmos-header').get(0), 
+        $('#cosmos-login .login-options').get(0), 
+    ], null);
 });
