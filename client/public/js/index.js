6c1bf02f9ad6ab90c8a8ec66139d4274dfe544dc
Derrick Liu <dliu@lediur.com>
2014-05-30 13:55:09 -0700

    Make landing page snazzy

---
+++ client/public/js/index.js
@@ -0,0 +1,49 @@
+function signInTransitionIn() {
+    // Run the exitContent animation and then the enterContent animation
+    // Use the recommended offset by leaving the offset argument empty to get the best performance
+    WinJS.UI.Animation.continuumForwardOut($("#login-container").get(0), null).done(function () {
+        $("#login-intro").css('display', 'none');
+        $("#login-providers").css('display', 'block');
+        return WinJS.UI.Animation.continuumForwardIn($("#login-container").get(0), null);
+    });
+}
+
+function signInGoBack() {
+    // Run the exitContent animation and then the enterContent animation
+    // Use the recommended offset by leaving the offset argument empty to get the best performance
+    WinJS.UI.Animation.continuumBackwardOut($("#login-container").get(0), null).done(function () {
+        $("#login-providers").css('display', 'none');
+        $("#login-intro").css('display', 'block');
+        return WinJS.UI.Animation.continuumBackwardIn($("#login-container").get(0), null);
+    });
+}
+
+function gameTransitionIn() {
+    WinJS.UI.Animation.continuumForwardOut($("#cosmos-landing").get(0), null).done(function () {
+        $("#cosmos-greeting").css('display', 'block');
+        return WinJS.UI.Animation.continuumForwardIn($("#cosmos-greeting").get(0), null).done(function () {
+            window.location.href = "/cosmos";
+        });
+    });
+}
+
+$(document).ready(function() {
+    if(window.location.hash) {
+        var hash = window.location.hash.substring(1);
+        if(hash === 'signin') {
+            signInTransitionIn();
+        }
+    }
+
+    $("#option-play").first().click(function() {
+        gameTransitionIn();
+    });
+
+    $("#option-signin").click(function() {
+        signInTransitionIn();
+    });
+
+    $("#login-providers .screen-back").first().click(function() {
+        signInGoBack();
+    });
+});
