303c6de0b495d1d33480a9bfae385296d13c357f
Eric Thong <ethong@stanford.edu>
2014-05-31 23:58:50 -0700

    Add docs for uuidGenerator

--- cosmos/levels/ExampleGraph-ck.js
+++
@@ -1,1 +0,0 @@
-var ExampleGraph=IgeSceneGraph.extend({classId:"ExampleGraph",addGraph:function(){},removeGraph:function(){}});typeof module!="undefined"&&typeof module.exports!="undefined"&&(module.exports=ExampleGraph);
