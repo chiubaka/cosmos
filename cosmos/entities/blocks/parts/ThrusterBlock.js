d616ba2916dfeac2ea7138508147f70afa9cee5a
Rafael Cosman <rafaelcosman@gmail.com>
2014-06-08 18:51:15 +0000

    ThrusterBlock.js comment edited online with Bitbucket

--- cosmos/entities/blocks/parts/ThrusterBlock.js
+++ cosmos/entities/blocks/parts/ThrusterBlock.js
@@ -2,6 +2,8 @@
  * Thruster blocks allow your ship to rotate.
  * They don't provide primary thrust, they just provide the little impulses neccesary to rotate your ship.
  * The more thrusters you have the more agile your ship (and your startup) will be.
+ * @class
+ * @namespace
  */
 var ThrusterBlock = Part.extend({
 	classId: 'ThrusterBlock',
