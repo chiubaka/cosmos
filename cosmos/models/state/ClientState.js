54befecc2fcee7fb56788fbfa48cc697841f08cf
Derrick Liu <dliu@lediur.com>
2014-06-12 04:49:17 -0700

    Document client state

--- cosmos/models/state/ClientState.js
+++ cosmos/models/state/ClientState.js
@@ -1,8 +1,23 @@
-﻿var ClientState = IgeClass.extend({
+﻿/**
+ * ClientState.js
+ * @author Derrick Liu
+ *
+ * The ClientState object manages the current client state and handles switching
+ * between different client game states and activating each states' associated capabilities.
+ *
+ * @class
+ * @typedef {Object} Capability
+ * @namespace  
+ */
+var ClientState = IgeClass.extend({
 	classId: 'ClientState',
 
 	/**
 	 * The possible selected caps and the states that they induce.
+	 * @type {Object}
+   * @memberof ClientState
+   * @instance
+   * @private
 	 */
 	capStates: {
 		'': 'idle',
@@ -12,6 +27,10 @@
 
 	/**
 	 * Capabilities associated with states above.
+	 * @type {Object}
+   * @memberof ClientState
+   * @instance
+   * @private
 	 */
 	capabilities: {
 		'mine': undefined,
@@ -19,17 +38,35 @@
 		'idle': undefined
 	},
 
+	/**
+	 * The currently selected capability identifier.
+	 * @type {string}
+   * @memberof ClientState
+   * @instance
+   * @private
+   */
 	_selectedCap: undefined,
 
+	/**
+	 * Initializes the capbar state handler and capabilities
+   * @memberof ClientState
+   * @instance
+   */
 	init: function() {
-		this.initEvents();
-		this.initCapabilities();
+		this._initEvents();
+		this._initCapabilities();
 
 		// Default selected capability
 		this.selectedCap('');
 	},
 
-	initEvents: function() {
+	/**
+	 * Initializes the capbar state handler and capabilities
+   * @memberof ClientState
+   * @instance
+   * @private
+   */
+	_initEvents: function() {
 		var self = this;
 
 		ige.on('capbar cap selected', function(classId) {
@@ -41,12 +78,25 @@
 		});
 	},
 
-	initCapabilities: function() {
+	/**
+	 * Initializes the capbar state handler and capabilities
+   * @memberof ClientState
+   * @instance
+   * @private
+   */
+	_initCapabilities: function() {
 		this.capabilities.mine = new MineCapability();
 		this.capabilities.construct = new ConstructCapability();
 		this.capabilities.idle = new Capability();
 	},
 
+	/**
+	 * Select the currently active capability by choosing a cap, or get the currently active capability's identifier
+	 * @param cap {string} the cap whose analogous capability to activate
+	 * @returns {string} the current capability state identifier
+	 * @memberof ClientState
+   * @instance
+   */
 	selectedCap: function(cap) {
 		if (cap !== undefined) {
 			if (this._selectedCap !== undefined) {
@@ -70,6 +120,12 @@
 	},
 
 	// TODO: Support multiple current capabilities (active, passive)
+	/**
+	 * Gets the actual capability object associated with the currently selected capability.
+	 * @returns {Object} the current capability's object
+	 * @memberof ClientState
+   * @instance
+   */
 	currentCapability: function() {
 		return this.capabilities[this.selectedCap()];
 	}
