690107464df1714a460deee62e34398236fdad8e
Daniel Chiu <daniechiu@gmail.com>
2014-06-23 17:39:42 -0700

    Add documentation and remove extraneous echo.

--- scripts/db/backupDatabase.sh
+++ scripts/db/backupDatabase.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# Creates a local backup of the remote database. The backup folder will be timestamped with the current time and date.
+# Please don't commit database backups to git!
+
 database_name="_cosmos_dev_db"
 output=$(date +%Y-%m-%d_%H.%M.%S)$database_name
-echo $output
 
 mongodump --host ds030827.mongolab.com --port 30827 -u cosmos-admin -p CS210-l3on1ne! --db cosmos-dev-db --out ${output}
 
