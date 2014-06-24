14a2a3a5682158af2847fd640b731aec56a92e34
Eric Thong <ethong@stanford.edu>
2014-06-23 23:23:55 -0700

    Remove tiles, bad performance with current way we're doing parallax and using the entity manager

--- cosmos/entities/scenery/StarfieldBackground.js
+++ cosmos/entities/scenery/StarfieldBackground.js
@@ -7,41 +7,15 @@
  */
 var StarfieldBackground = ParallaxBackground.extend({
 	classId: 'StarfieldBackground',
-	/**
-	 * TiledMap of starfield entities.
-	 * @type {IgeTileMap2d}
-	 * @memberof StarfieldBackground
-	 * @private
-	 * @instance
-	 */
-	_starTiles: undefined,
 
 	init: function () {
 		ParallaxBackground.prototype.init.call(this);
 
 		if (!ige.isServer) {
-
-			// Create a 3x3 tiled grid to make it easier to tile the background
-			// The grid is translated so the grid's (1,1) is at IGE location (0,0).
-			this._starTiles = new IgeTileMap2d()
-				.id('star_tiles')
-				.gridSize(3,3)
-				.tileWidth(6000)
-				.tileHeight(6000)
-				.translateTo(-9000, -9000, 0)
+			this.texture(ige.client.textures.background_starfield)
+				.width(6000)
+				.height(6000)
 				.mount(this);
-
-			// Fill the tiled grid with stars
-			for (var x = 0; x < 3; x++) {
-				for (var y = 0; y < 3; y++) {
-					new IgeEntity()
-						.mount(this._starTiles)
-						.widthByTile(1)
-						.heightByTile(1)
-						.texture(ige.client.textures.background_starfield)
-						.translateToTile(x, y, 0)
-				}
-			}
 		}
 	}
 });
