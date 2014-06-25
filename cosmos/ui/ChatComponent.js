35bd45cea406e5862739d89d79139b1796a76b1b
Daniel Chiu <daniechiu@gmail.com>
2014-06-25 16:03:21 -0700

    Add Candy.js for chat and restyle parts of it so that it plays nicely in a smaller window than it otherwise anticipates.

--- cosmos/ui/ChatComponent.js
+++ cosmos/ui/ChatComponent.js
@@ -2,7 +2,8 @@ var ChatComponent = IgeEventingClass.extend({
 	classId: 'ChatComponent',
 	componentId: 'chat',
 
-	element: undefined,
+	button: undefined,
+	chatClient: undefined,
 
 	init: function() {
 		var self = this;
@@ -14,12 +15,42 @@ var ChatComponent = IgeEventingClass.extend({
 
 		HUDComponent.loadHtml(ChatComponent.UI_ROOT + 'chat.html', function(html) {
 			bottomToolbar.append(html);
-			this.element = $('#chat');
+			self.button = $('#chat-button');
+
+			//ige.requireStylesheet(ChatComponent.CANDY_ROOT + 'res/default.css');
+
+			$.getScript(ChatComponent.CANDY_ROOT + 'libs/libs.min.js', function() {
+				$.getScript(ChatComponent.CANDY_ROOT + 'candy.min.js', function() {
+					Candy.init('http://tl-xmpp.cloudapp.net:5280/http-bind/', {
+						core: {
+							debug: true,
+							autojoin: ['test@conference.tl-xmpp.cloudapp.net']
+						},
+						view: { resources: ChatComponent.CANDY_ROOT + 'res/' }
+					});
+
+					Candy.Core.connect('dchiu@tl-xmpp.cloudapp.net', 'CS210-l3on1ne!');
+
+					self.chatClient = $('#candy');
+					self.chatClient.hide();
+
+					self.button.click(function(event) {
+						if (self.chatClient.is(':visible')) {
+							self.chatClient.hide();
+						}
+						else {
+							self.chatClient.show();
+						}
+					});
+				});
+			});
 		});
+
 	}
 });
 
 ChatComponent.UI_ROOT = '/components/chat/';
+ChatComponent.CANDY_ROOT = '/vendor/candy/'
 
 if (typeof(module) !== 'undefined' && typeof(module.expoerts) !== 'undefined') {
 	module.exports = ChatComponent;
