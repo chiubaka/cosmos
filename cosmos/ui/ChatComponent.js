f8fbdb0c49910ec202116aa74e453b2e3faf52e4
Daniel Chiu <daniechiu@gmail.com>
2014-06-25 18:42:47 -0700

    Chat now turns oranged and displays a number when there are unread messages.

--- cosmos/ui/ChatComponent.js
+++ cosmos/ui/ChatComponent.js
@@ -4,18 +4,23 @@ var ChatComponent = IgeEventingClass.extend({
 
 	button: undefined,
 	chatClient: undefined,
+	numUnread: undefined,
+	unreadLabel: undefined,
 
 	init: function() {
 		var self = this;
 		var bottomToolbar = $('#bottom-toolbar');
 		if (bottomToolbar.length === 0) {
-			this.log('Bottom toolbar has not been initialized.', 'error');
+			self.log('Bottom toolbar has not been initialized.', 'error');
 			return;
 		}
+
+		self.numUnread = 0;
 
 		HUDComponent.loadHtml(ChatComponent.UI_ROOT + 'chat.html', function(html) {
 			bottomToolbar.append(html);
 			self.button = $('#chat-button');
+			self.unreadLabel = $('#chat-unread-label');
 
 			//ige.requireStylesheet(ChatComponent.CANDY_ROOT + 'res/default.css');
 
@@ -23,7 +28,7 @@ var ChatComponent = IgeEventingClass.extend({
 				$.getScript(ChatComponent.CANDY_ROOT + 'candy.min.js', function() {
 					Candy.init('http://tl-xmpp.cloudapp.net:5280/http-bind/', {
 						core: {
-							debug: true,
+							debug: false,
 							autojoin: ['test@conference.tl-xmpp.cloudapp.net']
 						},
 						view: { resources: ChatComponent.CANDY_ROOT + 'res/' }
@@ -32,6 +37,13 @@ var ChatComponent = IgeEventingClass.extend({
 					Candy.Core.connect('dchiu@tl-xmpp.cloudapp.net', 'CS210-l3on1ne!');
 
 					self.chatClient = $('#candy');
+
+					$(Candy).on('candy:core.message', function(evt, args) {
+						if (args.timestamp === undefined && self.chatClient.is(':hidden')) {
+							self.incrementUnread();
+						}
+					});
+
 					self.chatClient.hide();
 
 					self.button.click(function(event) {
@@ -40,12 +52,31 @@ var ChatComponent = IgeEventingClass.extend({
 						}
 						else {
 							self.chatClient.show();
+							self.clearUnread();
 						}
 					});
 				});
 			});
 		});
+	},
 
+	incrementUnread: function() {
+		this.button.addClass('unread');
+		this.numUnread++;
+		this.updateLabel();
+	},
+
+	clearUnread: function() {
+		this.button.removeClass('unread');
+		this.numUnread = 0;
+		this.updateLabel();
+	},
+
+	updateLabel: function() {
+		this.unreadLabel.text(this.numUnread);
+
+		var numChars = this.numUnread.toString().length;
+		this.button.css('font-size', '' + (85 - (numChars - 1) * 20) + '%');
 	}
 });
 
