3a654e154ae25ab764d4106920df3f454d5fddb2
Daniel Chiu <daniechiu@gmail.com>
2014-07-01 23:00:44 -0700

    Change username to lowercase when searching database so that different capitlizations of the same name are not considered valid.

--- cosmos/entities/player/Player.js
+++ cosmos/entities/player/Player.js
@@ -624,7 +624,9 @@ Player.onUsernameRequested = function(username, clientId) {
 		ige.network.send('cosmos:player.username.set.error', 'Alphanumeric characters and underscores only', clientId);
 	}
 
-	DbPlayer.findByUsername(username, function(err, foundPlayer) {
+	// Find users with this name. Change username to lowercase for search to make sure that we don't allow different
+	// capitalizations of the same name.
+	DbPlayer.findByUsername(username.toLowerCase(), function(err, foundPlayer) {
 		if (err) {
 			console.error('Error finding player with username ' + username + '. Error: ' + err);
 			ige.network.send('cosmos:player.username.set.error', 'Database error', clientId);
