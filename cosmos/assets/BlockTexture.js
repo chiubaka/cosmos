2d75129dd468f7b90a841c0131b58c5aeb00d96e
Daniel Chiu <daniechiu@gmail.com>
2014-07-02 17:07:08 -0700

    Switch block smart texture rendering from paths to drawing rectangles, which eliminates the error in drawing block borders when including line widths.

--- cosmos/assets/BlockTexture.js
+++ cosmos/assets/BlockTexture.js
@@ -3,31 +3,23 @@ var image = {
 	render: function(ctx, entity) {
 		ctx.globalCompositeOperation = "source-over";
 
-		// Width of the outline
-		ctx.lineWidth = Block.WIDTH / 12;
-
 		// Draw block background
 		if (entity.textureBackground !== undefined) {
 			ctx.fillStyle = entity.textureBackground;
-			ctx.beginPath();
-			ctx.moveTo(-entity._bounds2d.x2, -entity._bounds2d.y2);
-			ctx.lineTo(entity._bounds2d.x2, -entity._bounds2d.y2);
-			ctx.lineTo(entity._bounds2d.x2, entity._bounds2d.y2);
-			ctx.lineTo(-entity._bounds2d.x2, entity._bounds2d.y2);
-			ctx.lineTo(-entity._bounds2d.x2, -entity._bounds2d.y2);
-			ctx.fill();
+			ctx.fillRect(-entity._bounds2d.x2, -entity._bounds2d.y2, entity.width(), entity.height());
 		}
+
+		// Width of the outline
+		ctx.lineWidth = 1;
 
 		// Draw block outline
 		if (entity.textureOutline !== undefined) {
 			ctx.strokeStyle = entity.textureOutline;
-			ctx.beginPath();
-			ctx.moveTo(-entity._bounds2d.x2, -entity._bounds2d.y2);
-			ctx.lineTo(entity._bounds2d.x2, -entity._bounds2d.y2);
-			ctx.lineTo(entity._bounds2d.x2, entity._bounds2d.y2);
-			ctx.lineTo(-entity._bounds2d.x2, entity._bounds2d.y2);
-			ctx.lineTo(-entity._bounds2d.x2, -entity._bounds2d.y2);
-			ctx.stroke();
+			ctx.strokeRect(-entity._bounds2d.x2 + ctx.lineWidth / 2,
+					-entity._bounds2d.x2 + ctx.lineWidth / 2,
+					entity.width() - ctx.lineWidth,
+					entity.height() - ctx.lineWidth
+			);
 		}
 
 		// Draw block icon
