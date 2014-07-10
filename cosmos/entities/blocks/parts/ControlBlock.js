0addebceb6a1e1e8f6f2061575e5c6741014a3f5
Eric Thong <ethong@stanford.edu>
2014-07-09 23:40:00 -0700

    SVG textures are now loaded like IgeTextures, remove setTimeout hacks

--- cosmos/entities/blocks/parts/ControlBlock.js
+++ cosmos/entities/blocks/parts/ControlBlock.js
@@ -24,7 +24,7 @@ var ControlBlock = Part.extend({
 		if (!ige.isServer) {
 			this.textureBackground = "rgb(242, 242, 242)";
 			this.textureOutline = "rgb(8, 143, 255)";
-			this.textureSvg = ige.client.svgs.control;
+			this.textureSvg = ige.client.textures.control;
 		}
 	}
 });
