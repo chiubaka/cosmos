423158725cf615cff58b10fb489c369f01ce5d3c
Daniel Chiu <daniechiu@gmail.com>
2014-07-31 23:51:18 -0700

    Move PixiRenderableComponent in LaserParticle.

--- cosmos/entities/effects/mining/LaserParticle.js
+++ cosmos/entities/effects/mining/LaserParticle.js
@@ -23,11 +23,6 @@ var LaserParticle = IgeParticle.extend({
 		this._emitter = emitter;
 		IgeParticle.prototype.init.call(this);
 
-
-		this.texture(ige.client.textures.rectangleTexture)
-			.width(7)
-			.height(7)
-
 		this.addComponent(ParticleRenderableComponent, {createDisplayObject: function() {
 			var particle = new PIXI.Graphics();
 			particle.beginFill(0xff5a00);
@@ -36,6 +31,10 @@ var LaserParticle = IgeParticle.extend({
 
 			return particle;
 		}});
+
+		this.texture(ige.client.textures.rectangleTexture)
+			.width(7)
+			.height(7);
 	},
 
 });
