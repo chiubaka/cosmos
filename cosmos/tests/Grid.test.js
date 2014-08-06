2192f52f9b946bb00454e459a9ba09c58ad9138f
Daniel Chiu <daniechiu@gmail.com>
2014-08-06 16:08:52 -0700

    Add test to check if the previous object is returned when remove is called on a Grid.

--- cosmos/tests/Grid.test.js
+++ cosmos/tests/Grid.test.js
@@ -1,7 +1,8 @@
 /**
  * Tests an object that has the Grid interface.
- * @param beforeEachFunc {function} The function to run before each Grid interface test. This function must, at minimum,
- * create an object and assign it to this.grid and assign an array of objects to this.testObjects.
+ * @param beforeEachFunc {function} The function to run before each Grid interface test. This
+ * function must, at minimum, create an object and assign it to this.grid and assign an array of
+ * objects to this.testObjects.
  * @param afterEachFunc {function} The function to run after each Grid interface test.
  */
 var testGrid = function(beforeEachFunc, afterEachFunc) {
@@ -34,14 +35,19 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 				this.grid.has(undefined);
 			});
 
-			it("should fail gracefully if no location is passed to the remove function.", function() {
-				this.grid.remove(undefined);
-			});
+			it("should fail gracefully if no location is passed to the remove function.",
+				function() {
+					this.grid.remove(undefined);
+				}
+			);
 		});
 
-		it("should return undefined if asked to retrieve an object at a location that is unoccupied.", function() {
-			expect(this.grid.get(new GridLocation(10, 10))).not.toBeDefined();
-		});
+		it("should return undefined if asked to retrieve an object at a location that is " +
+			"unoccupied.",
+			function() {
+				expect(this.grid.get(new GridLocation(10, 10))).not.toBeDefined();
+			}
+		);
 
 		it("should be able to tell whether or not a given location is occupied.", function() {
 			expect(this.grid.has(new GridLocation(0, 0))).toBe(false);
@@ -58,6 +64,15 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 			this.grid.put(this.testObjects[0], new GridLocation(-1, -1));
 			expect(this.grid.get(new GridLocation(-1, -1))).toBe(this.testObjects[0]);
 		});
+
+		it("should return the previous object if remove is called on an occupied location.",
+			function() {
+				this.grid.put(this.testObjects[0], new GridLocation(0, 0));
+				this.grid.put(this.testObjects[1], new GridLocation(0, 1));
+				expect(this.grid.remove(new GridLocation(0, 0))).toBe(this.testObjects[0]);
+				expect(this.grid.remove(new GridLocation(0, 1))).toBe(this.testObjects[1]);
+			}
+		);
 
 		it("should not be able to retrieve an object that has been removed from it.", function() {
 			this.grid.put(this.testObjects[0], new GridLocation(0, 0));
