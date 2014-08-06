07e641498da4c4aa4f9065f564581d401aaff685
Daniel Chiu <daniechiu@gmail.com>
2014-08-06 12:18:47 -0700

    Implement the add and get functions in the SparseGrid class.

--- cosmos/entities/SparseGrid.js
+++ cosmos/entities/SparseGrid.js
@@ -1,30 +1,61 @@
 var SparseGrid = function() {
 	var grid = {};
 
-	this.add = function(object, location) {
+	this.add = function(object, loc) {
 		if (object === undefined) {
 			console.warn("SparseGrid#add: no object parameter to add.");
 			return;
 		}
-		if (!GridLocation.validateLocation(location)) {
-			console.warn("SparseGrid#add: no location provided.");
+		if (!GridLocation.validateLocation(loc)) {
+			console.warn("SparseGrid#add: no loc provided.");
+			return;
+		}
+
+		if (!hasRowInCol(loc)) {
+			createRowInCol(loc);
+		}
+
+		grid[loc.row][loc.col] = object;
+	};
+
+	this.get = function(loc) {
+		if (!GridLocation.validateLocation(loc)) {
+			console.warn("SparseGrid#get: no loc provided.");
+			return;
+		}
+		if (!hasCol(loc.col) || !hasRowInCol(loc)) {
+			return;
+		}
+
+		return grid[loc.col][loc.row];
+	};
+
+	this.remove = function(loc) {
+		if (!GridLocation.validateLocation(loc)) {
+			console.warn("SparseGrid#remove: no loc provided.");
 			return;
 		}
 	};
 
-	this.get = function(location) {
-		if (!GridLocation.validateLocation(location)) {
-			console.warn("SparseGrid#get: no location provided.");
-			return;
-		}
-	};
+	function hasRowInCol(loc) {
+		return grid[loc.col] !== undefined && grid[loc.col][loc.row] !== undefined;
+	}
 
-	this.remove = function(location) {
-		if (!GridLocation.validateLocation(location)) {
-			console.warn("SparseGrid#remove: no location provided.");
-			return;
+	function createRowInCol(loc) {
+		if (!hasCol(loc.col)) {
+			createCol(loc.col);
 		}
-	};
+
+		grid[loc.col][loc.row] = {};
+	}
+
+	function hasCol(col) {
+		return grid[col] !== undefined;
+	}
+
+	function createCol(col) {
+		grid[col] = {};
+	}
 };
 
 if (typeof(module) !== "undefined" && typeof(module.exports) !== "undefined") { module.exports = SparseGrid; }
