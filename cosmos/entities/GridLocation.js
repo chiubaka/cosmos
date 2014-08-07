3d7525f0bc861a5dcaebdfe84416bc41b062bc31
Daniel Chiu <daniechiu@gmail.com>
2014-08-06 17:40:07 -0700

    Add tests for GridLocation#subtract and GridLocation#copy.

--- cosmos/entities/GridLocation.js
+++ cosmos/entities/GridLocation.js
@@ -4,10 +4,25 @@ var GridLocation = function(col, row) {
 };
 
 GridLocation.copy = function(location) {
+	if (!GridLocation.validateLocation(location)) {
+		console.warn("Invalid location passed to GridLocation#copy.");
+		return;
+	}
+
 	return new GridLocation(location.col, location.row);
 };
 
 GridLocation.subtract = function(location1, location2) {
+	if (!GridLocation.validateLocation(location1)) {
+		console.warn("Location 1 passed to GridLocation#subtract is invalid.");
+		return;
+	}
+
+	if (!GridLocation.validateLocation(location2)) {
+		console.warn("Location 2 passed to GridLocation#subtract is invalid.");
+		return;
+	}
+
 	return new GridLocation(location1.col - location2.col, location1.row - location2.row);
 };
 
--- cosmos/tests/GridLocation.test.js
+++ cosmos/tests/GridLocation.test.js
@@ -1,4 +1,33 @@
 describe("A GridLocation", function() {
+	describe("should have public interface functions that fails gracefully", function() {
+		beforeEach(function() {
+			TestUtils.disableLogging();
+		});
+
+		afterEach(function() {
+			TestUtils.enableLogging();
+		});
+
+		it("when no location is passed to the copy function.", function() {
+			var copy = GridLocation.copy();
+			expect(copy).not.toBeDefined();
+		});
+
+		it("when one or both of the locations is not passed to the subtract function.", function() {
+			var location1 = new GridLocation(-9001, -9001);
+			var location2 = new GridLocation(1234, 1234);
+
+			var result = GridLocation.subtract();
+			expect(result).not.toBeDefined();
+
+			result = GridLocation.subtract(location1);
+			expect(result).not.toBeDefined();
+
+			result = GridLocation.subtract(undefined, location2);
+			expect(result).not.toBeDefined();
+		});
+	});
+
 	it("is not valid if it is undefined.", function() {
 		expect(GridLocation.validateLocation()).toBe(false);
 	});
@@ -17,5 +46,19 @@ describe("A GridLocation", function() {
 
 	it("can handle negative numbers.", function() {
 		expect(GridLocation.validateLocation(new GridLocation(-1, -5))).toBe(true);
+	});
+
+	it("can be subtracted from another GridLocation.", function() {
+		var location1 = new GridLocation(20, 15);
+		var location2 = new GridLocation(5, 10);
+		expect(GridLocation.subtract(location1, location2)).toEqual(new GridLocation(15, 5));
+		expect(GridLocation.subtract(location2, location1)).toEqual(new GridLocation(-15, -5));
+	});
+
+	it("can be copied.", function() {
+		var location = new GridLocation(4, 5);
+		var copy = GridLocation.copy(location);
+		expect(location).not.toBe(copy);
+		expect(location).toEqual(copy);
 	});
 });
