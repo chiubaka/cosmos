f4ff32c4f75ec8a200a3fb876702bc231c7d8555
Daniel Chiu <daniechiu@gmail.com>
2014-08-07 22:58:54 -0700

    Move all tests that touch private variables out of the common Grid.test.js file.

--- cosmos/tests/Grid.test.js
+++ cosmos/tests/Grid.test.js
@@ -1,14 +1,20 @@
 /**
- * Tests an object that has the Grid interface.
- * @param beforeEachFunc {function} The function to run before each Grid interface test. This
+ * Tests an object that has the Grid interface. If no before or after function is provided, it is
+ * expected that the parent describe sets up all of the necessary test objects.
+ * @param beforeEachFunc {function?} The function to run before each Grid interface test. This
  * function must, at minimum, create an object and assign it to this.grid and assign an array of
  * objects to this.testObjects.
- * @param afterEachFunc {function} The function to run after each Grid interface test.
+ * @param afterEachFunc {function?} The function to run after each Grid interface test.
  */
 var testGrid = function(beforeEachFunc, afterEachFunc) {
 	describe("implementing the Grid interface", function() {
-		beforeEach(beforeEachFunc);
-		afterEach(afterEachFunc);
+		if (typeof(beforeEachFunc) === "function") {
+			beforeEach(beforeEachFunc);
+		}
+
+		if (typeof(afterEachFunc) === "function") {
+			afterEach(afterEachFunc);
+		}
 
 		describe("should have public interface functions that fails gracefully", function() {
 			beforeEach(function() {
@@ -101,18 +107,15 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 		it("should be able to put objects inside of itself.", function() {
 			expect(this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(0, 0), false))
 				.toEqual([]);
-			expect(this.grid._grid[0][0]).toBe(this.testObjects["1x1"][0]);
-			expect(this.grid._colCounts[0]).toEqual(1);
+			expect(this.grid.get(new IgePoint2d(0, 0))).toEqual([this.testObjects["1x1"][0]]);
 			expect(this.grid.count()).toEqual(1);
 
 			expect(this.grid.put(this.testObjects["2x2"][0], new IgePoint2d(10, 10), false))
 				.toEqual([]);
 			for (var x = 10; x < 12; x++) {
 				for (var y = 10; y < 12; y++) {
-					expect(this.grid._grid[x][y]).toBe(this.testObjects["2x2"][0]);
+					expect(this.grid.get(new IgePoint2d(x, y))).toEqual([this.testObjects["2x2"][0]]);
 				}
-				expect(this.grid._colCounts[x]).toEqual(2);
-				expect(this.grid._colCounts[x]).toEqual(2);
 			}
 			expect(this.grid.count()).toEqual(2);
 		});
@@ -137,10 +140,8 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 
 			for (var x = -5; x < -3; x++) {
 				for (var y = -3; y < -1; y++) {
-					expect(this.grid._grid[x][y]).toBe(this.testObjects["2x2"][0]);
+					expect(this.grid.get(new IgePoint2d(x, y))).toEqual([this.testObjects["2x2"][0]]);
 				}
-				expect(this.grid._colCounts[x]).toEqual(2);
-				expect(this.grid._colCounts[x]).toEqual(2);
 			}
 			expect(this.grid.count()).toEqual(1);
 		});
@@ -175,21 +176,6 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 			expect(this.grid.has(new IgePoint2d(8, 8), 2, 2)).toBe(true);
 		});
 
-		it("should have references to a large object at each location that it occupies.",
-			function() {
-				expect(this.grid.put(this.testObjects["3x3"][0], new IgePoint2d(10, 10), false))
-					.toEqual([]);
-				for (var x = 10; x < 13; x++) {
-					for (var y = 10; y < 13; y++) {
-						expect(this.grid._grid[x][y]).toBe(this.testObjects["3x3"][0]);
-					}
-					expect(this.grid._colCounts[x]).toEqual(3);
-					expect(this.grid._colCounts[x]).toEqual(3);
-				}
-				expect(this.grid.count()).toEqual(1);
-			}
-		);
-
 		it("should be able to retrieve objects that have been placed inside of it.", function() {
 			expect(this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(5, 5), false))
 				.toEqual([]);
@@ -206,8 +192,6 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 						expect(this.grid.get(new IgePoint2d(x, y)))
 							.toEqual([this.testObjects["3x3"][0]]);
 					}
-					expect(this.grid._colCounts[x]).toEqual(3);
-					expect(this.grid._colCounts[x]).toEqual(3);
 				}
 				expect(this.grid.count()).toEqual(1);
 			}
@@ -254,13 +238,6 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 			expect(this.grid.get(new IgePoint2d(0, 0))).toEqual([this.testObjects["1x1"][0]]);
 			this.grid.remove(new IgePoint2d(0, 0));
 			expect(this.grid.get(new IgePoint2d(0, 0))).toEqual([]);
-		});
-
-		it("should erase references to removed objects.", function() {
-			this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(0, 0), true);
-			this.grid.remove(new IgePoint2d(0, 0));
-			expect(this.grid._grid[0]).not.toBeDefined();
-
 		});
 
 		it("should do nothing if asked to remove an object at an unoccupied location.", function() {
@@ -372,36 +349,6 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 			}
 		);
 
-		it("should clear all locations that an object occupied when that object is removed.",
-			function() {
-				expect(this.grid.put(this.testObjects["3x3"][0], new IgePoint2d(10, 10), true))
-					.toEqual([]);
-
-				expect(this.grid.remove(new IgePoint2d(10, 10))).toEqual([this.testObjects["3x3"][0]]);
-
-				for (var x = 10; x < 13; x++) {
-					for (var y = 10; y < 13; y++) {
-						if (this.grid._grid[x]) {
-							expect(this.grid._grid[x][y]).not.toBeDefined();
-						}
-					}
-				}
-
-				expect(this.grid.put(this.testObjects["3x3"][0], new IgePoint2d(10, 10), true))
-					.toEqual([]);
-
-				expect(this.grid.remove(new IgePoint2d(10, 10), 2, 2)).toEqual([this.testObjects["3x3"][0]]);
-
-				for (var x = 10; x < 13; x++) {
-					for (var y = 10; y < 13; y++) {
-						if (this.grid._grid[x]) {
-							expect(this.grid._grid[x][y]).not.toBeDefined();
-						}
-					}
-				}
-			}
-		);
-
 		it("should return an empty list when removing from a location that is unoccupied.",
 			function() {
 				expect(this.grid.remove(new IgePoint2d(0, 0))).toEqual([]);
@@ -413,26 +360,6 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 				TestUtils.disableLogging();
 				expect(this.grid.has()).toBe(false);
 				TestUtils.enableLogging();
-			}
-		);
-
-		it("should clear all locations that displaced objects occupied when a new object is " +
-			"added.",
-			function() {
-				this.grid.put(this.testObjects["2x2"][0], new IgePoint2d(100, 100), false);
-				expect(this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(100, 100), true))
-					.toEqual([this.testObjects["2x2"][0]]);
-
-				for (var x = 100; x < 102; x++) {
-					for (var y = 100; y < 102; y++) {
-						if (x === 100 && y === 100) {
-							continue;
-						}
-						if (this.grid._grid[x]) {
-							expect(this.grid._grid[x][y]).not.toBeDefined();
-						}
-					}
-				}
 			}
 		);
 	});
--- cosmos/tests/SparseGrid.test.js
+++ cosmos/tests/SparseGrid.test.js
@@ -8,31 +8,101 @@ var TestGridObject = IgeEntity.extend({
 });
 
 describe("A SparseGrid", function() {
-	testGrid(
-		// beforeEachFunc
+	function beforeEachFunc() {
+		this.grid = new SparseGrid();
+		this.testObjects = {};
+		this.testObjects["1x1"] = [
+			new TestGridObject(1, 1),
+			new TestGridObject(1, 1),
+			new TestGridObject(1, 1),
+			new TestGridObject(1, 1)
+		];
+		this.testObjects["2x2"] = [
+			new TestGridObject(2, 2),
+			new TestGridObject(2, 2),
+			new TestGridObject(2, 2),
+			new TestGridObject(2, 2)
+		];
+		this.testObjects["3x3"] = [
+			new TestGridObject(3, 3),
+			new TestGridObject(3, 3),
+			new TestGridObject(3, 3),
+			new TestGridObject(3, 3)
+		];
+	};
+
+	beforeEach(beforeEachFunc);
+
+	testGrid();
+
+	it("should have references to a large object at each location that it occupies.",
 		function() {
-			this.grid = new SparseGrid();
-			this.testObjects = {};
-			this.testObjects["1x1"] = [
-				new TestGridObject(1, 1),
-				new TestGridObject(1, 1),
-				new TestGridObject(1, 1),
-				new TestGridObject(1, 1)
-			];
-			this.testObjects["2x2"] = [
-				new TestGridObject(2, 2),
-				new TestGridObject(2, 2),
-				new TestGridObject(2, 2),
-				new TestGridObject(2, 2)
-			];
-			this.testObjects["3x3"] = [
-				new TestGridObject(3, 3),
-				new TestGridObject(3, 3),
-				new TestGridObject(3, 3),
-				new TestGridObject(3, 3)
-			];
-		},
-		// afterEachFunc
-		function() {}
+			expect(this.grid.put(this.testObjects["3x3"][0], new IgePoint2d(10, 10), false))
+				.toEqual([]);
+			for (var x = 10; x < 13; x++) {
+				for (var y = 10; y < 13; y++) {
+					expect(this.grid._grid[x][y]).toBe(this.testObjects["3x3"][0]);
+				}
+				expect(this.grid._colCounts[x]).toEqual(3);
+				expect(this.grid._colCounts[x]).toEqual(3);
+			}
+			expect(this.grid.count()).toEqual(1);
+		}
+	);
+
+	it("should erase references to removed objects.", function() {
+		this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(0, 0), true);
+		this.grid.remove(new IgePoint2d(0, 0));
+		expect(this.grid._grid[0]).not.toBeDefined();
+	});
+
+	it("should clear all locations that an object occupied when that object is removed.",
+		function() {
+			expect(this.grid.put(this.testObjects["3x3"][0], new IgePoint2d(10, 10), true))
+				.toEqual([]);
+
+			expect(this.grid.remove(new IgePoint2d(10, 10))).toEqual([this.testObjects["3x3"][0]]);
+
+			for (var x = 10; x < 13; x++) {
+				for (var y = 10; y < 13; y++) {
+					if (this.grid._grid[x]) {
+						expect(this.grid._grid[x][y]).not.toBeDefined();
+					}
+				}
+			}
+
+			expect(this.grid.put(this.testObjects["3x3"][0], new IgePoint2d(10, 10), true))
+				.toEqual([]);
+
+			expect(this.grid.remove(new IgePoint2d(10, 10), 2, 2)).toEqual([this.testObjects["3x3"][0]]);
+
+			for (var x = 10; x < 13; x++) {
+				for (var y = 10; y < 13; y++) {
+					if (this.grid._grid[x]) {
+						expect(this.grid._grid[x][y]).not.toBeDefined();
+					}
+				}
+			}
+		}
+	);
+
+	it("should clear all locations that displaced objects occupied when a new object is " +
+			"added.",
+		function() {
+			this.grid.put(this.testObjects["2x2"][0], new IgePoint2d(100, 100), false);
+			expect(this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(100, 100), true))
+				.toEqual([this.testObjects["2x2"][0]]);
+
+			for (var x = 100; x < 102; x++) {
+				for (var y = 100; y < 102; y++) {
+					if (x === 100 && y === 100) {
+						continue;
+					}
+					if (this.grid._grid[x]) {
+						expect(this.grid._grid[x][y]).not.toBeDefined();
+					}
+				}
+			}
+		}
 	);
 });
