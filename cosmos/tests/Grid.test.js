a968dfb52f7a0b48f2e1af05c449b4611f57a0c5
Daniel Chiu <daniechiu@gmail.com>
2014-08-07 21:43:21 -0700

    Added graceful interface tests.

--- cosmos/tests/Grid.test.js
+++ cosmos/tests/Grid.test.js
@@ -10,7 +10,7 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 		beforeEach(beforeEachFunc);
 		afterEach(afterEachFunc);
 
-		/*(describe("should have public interface functions that fails gracefully", function() {
+		describe("should have public interface functions that fails gracefully", function() {
 			beforeEach(function() {
 				TestUtils.disableLogging();
 			});
@@ -20,19 +20,45 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 			});
 
 			it("when no object is passed to the put function.", function() {
-				this.grid.put(undefined, new IgePoint2d(0, 0));
+				this.grid.put(undefined, new IgePoint2d(0, 0), true);
 			});
 
 			it("when no location is passed to the put function.", function() {
-				this.grid.put(this.testObjects[0], undefined);
+				this.grid.put(this.testObjects["1x1"][0], undefined, true);
 			});
+
+			it("when no replace value is passed to the put function.", function() {
+				this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(0, 0));
+			});
+
+			it("when a location without the GridData component is passed to the put function.",
+				function() {
+					this.grid.put(this.testObjects["1x1"][0], {x: 0, y: 0}, true);
+				}
+			);
 
 			it("when no location is passed to the get function.", function() {
 				this.grid.get(undefined);
 			});
 
+			it("when no width is passed to the get function.", function() {
+				this.grid.get(new IgePoint2d(0, 0), undefined, 4);
+			});
+
+			it("when no height is passed to the get function.", function() {
+				this.grid.get(new IgePoint2d(0, 0), 4, undefined);
+			});
+
 			it("when no location is passed to the has function.", function() {
 				this.grid.has(undefined);
+			});
+
+			it("when no width is passed to the has function.", function() {
+				this.grid.has(new IgePoint2d(0, 0), undefined, 4);
+			});
+
+			it("when no height is passed to the has function.", function() {
+				this.grid.has(new IgePoint2d(0, 0), 4, undefined);
 			});
 
 			it("when no location is passed to the remove function.",
@@ -40,101 +66,19 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 					this.grid.remove(undefined);
 				}
 			);
+
+			it("when no width is passed to the remove function.",
+				function() {
+					this.grid.remove(new IgePoint2d(0, 0), undefined, 3);
+				}
+			);
+
+			it("when no height is passed to the remove function.",
+				function() {
+					this.grid.remove(new IgePoint2d(0, 0), 3, undefined);
+				}
+			);
 		});
-
-		it("should return undefined if asked to retrieve an object at a location that is " +
-			"unoccupied.",
-			function() {
-				expect(this.grid.get(new IgePoint2d(10, 10))).not.toBeDefined();
-			}
-		);
-
-		it("should report that a location is unoccupied if asked about an invalid location.",
-			function() {
-				TestUtils.disableLogging();
-				expect(this.grid.has()).toBe(false);
-				TestUtils.enableLogging();
-			}
-		);
-
-		it("should be able to retrieve an object that has been placed in it.", function() {
-			this.grid.put(this.testObjects[0], new IgePoint2d(0, 0));
-			expect(this.grid.get(new IgePoint2d(0, 0))).toBe(this.testObjects[0]);
-		});
-
-		it("should return the previous object if remove is called on an occupied location.",
-			function() {
-				this.grid.put(this.testObjects[0], new IgePoint2d(0, 0));
-				this.grid.put(this.testObjects[1], new IgePoint2d(0, 1));
-				expect(this.grid.remove(new IgePoint2d(0, 0))).toBe(this.testObjects[0]);
-				expect(this.grid.remove(new IgePoint2d(0, 1))).toBe(this.testObjects[1]);
-			}
-		);
-
-		it("should not be able to retrieve an object that has been removed from it.", function() {
-			this.grid.put(this.testObjects[0], new IgePoint2d(0, 0));
-			expect(this.grid.get(new IgePoint2d(0, 0))).toBe(this.testObjects[0]);
-			this.grid.remove(new IgePoint2d(0, 0));
-			expect(this.grid.get(new IgePoint2d(0, 0))).not.toBeDefined();
-		});
-
-		it("should do nothing if asked to remove an object at an unoccupied location.", function() {
-			this.grid.remove(new IgePoint2d(0, 0));
-			expect(this.grid.count()).toBe(0);
-		});
-
-		it("should keep track of the number of objects it contains.", function() {
-			expect(this.grid.count()).toBe(0);
-			this.grid.put(this.testObjects[0], new IgePoint2d(0, 0));
-			expect(this.grid.count()).toBe(1);
-			this.grid.put(this.testObjects[0], new IgePoint2d(0, 1));
-			expect(this.grid.count()).toBe(2);
-			this.grid.remove(new IgePoint2d(0, 0));
-			expect(this.grid.count()).toBe(1);
-			this.grid.remove(new IgePoint2d(0, 0));
-			expect(this.grid.count()).toBe(1);
-			this.grid.remove(new IgePoint2d(0, 1));
-			expect(this.grid.count()).toBe(0);
-		});
-
-		it("should replace the existing object and return it if asked to place an object at an" +
-			"occupied location",
-			function() {
-				this.grid.put(this.testObjects[0], new IgePoint2d(0, 0));
-				expect(this.grid.put(this.testObjects[1], new IgePoint2d(0, 0)))
-					.toBe(this.testObjects[0]);
-				expect(this.grid.get(new IgePoint2d(0, 0))).toBe(this.testObjects[1]);
-			}
-		);
-
-		it("should be able to handle hundreds of objects.", function() {
-			// Num objects must be even or the test will break.
-			var numObjects = 100;
-
-			// Add numObjects objects to the grid.
-			for (var i = 0; i < numObjects; i++) {
-				var object = this.testObjects[i % this.testObjects.length];
-				this.grid.put(object, new IgePoint2d(i, i));
-			}
-			expect(this.grid.count()).toBe(numObjects);
-
-			// Remove every other object.
-			for (var i = 0; i < numObjects; i += 2) {
-				this.grid.remove(new IgePoint2d(i, i));
-			}
-			expect(this.grid.count()).toBe(numObjects / 2);
-
-			// Check that the remaining locations are still filled.
-			for (var i = 1; i < numObjects; i += 2) {
-				var object = this.testObjects[i % this.testObjects.length];
-				expect(this.grid.get(new IgePoint2d(i, i))).toBe(object);
-			}
-
-			// Check that the remove objects really were removed.
-			for (var i = 0; i < numObjects; i += 2) {
-				expect(this.grid.get(new IgePoint2d(i, i))).not.toBeDefined();
-			}
-		});*/
 
 		it("should be able to put objects inside of itself.", function() {
 			expect(this.grid.put(this.testObjects["1x1"][0], new IgePoint2d(0, 0), false))
@@ -440,6 +384,20 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 			}
 		);
 
+		it("should return an empty list when removing from a location that is unoccupied.",
+			function() {
+				expect(this.grid.remove(new IgePoint2d(0, 0))).toEqual([]);
+			}
+		);
+
+		it("should report that a location is unoccupied if asked about an invalid location.",
+			function() {
+				TestUtils.disableLogging();
+				expect(this.grid.has()).toBe(false);
+				TestUtils.enableLogging();
+			}
+		);
+
 		it("should clear all locations that displaced objects occupied when a new object is " +
 			"added.",
 			function() {
@@ -459,34 +417,5 @@ var testGrid = function(beforeEachFunc, afterEachFunc) {
 				}
 			}
 		);
-
-		xit("should be able to handle hundreds of objects.", function() {
-			// Num objects must be even or the test will break.
-			var numObjects = 200;
-
-			// Add numObjects objects to the grid.
-			for (var i = 0; i < numObjects; i++) {
-				var object = this.testObjects[i % this.testObjects.length];
-				this.grid.put(object, new IgePoint2d(i, i));
-			}
-			expect(this.grid.count()).toBe(numObjects);
-
-			// Remove every other object.
-			for (var i = 0; i < numObjects; i += 2) {
-				this.grid.remove(new IgePoint2d(i, i));
-			}
-			expect(this.grid.count()).toBe(numObjects / 2);
-
-			// Check that the remaining locations are still filled.
-			for (var i = 1; i < numObjects; i += 2) {
-				var object = this.testObjects[i % this.testObjects.length];
-				expect(this.grid.get(new IgePoint2d(i, i))).toBe(object);
-			}
-
-			// Check that the remove objects really were removed.
-			for (var i = 0; i < numObjects; i += 2) {
-				expect(this.grid.get(new IgePoint2d(i, i))).not.toBeDefined();
-			}
-		});
 	});
 };
