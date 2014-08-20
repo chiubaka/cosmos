f988db670b8980bdfa61e95c9af2282c747ad025
Daniel Chiu <daniechiu@gmail.com>
2014-08-20 16:39:03 -0700

    Add Element#fromType function which creates an element with a specified size and classId.

--- cosmos/entities/blocks/Element.js
+++ cosmos/entities/blocks/Element.js
@@ -14,4 +14,27 @@ var Element = Block.extend({
 	}
 });
 
+/**
+ * Instantiates an element block of a specified size from a type. Will return undefined if asked
+ * to instantiate a block that is not an element.
+ * @param type {String} The classId string of the element to create.
+ * @param dimensions {Object} An object with two properties: {gridWidth: {number}, gridHeight:
+ * {number}}.
+ * Specifies how many rows and columns this block takes up in the grid.
+ * @returns {undefined}
+ */
+Element.fromType = function(type, dimensions) {
+	if (cosmos.blocks.constructors[type] === undefined) {
+		return undefined;
+	}
+	if (!(cosmos.blocks.instances[type] instanceof Element)) {
+		console.warn("Element#fromType: attempted to instantiated a non-element block type.");
+		return undefined;
+	}
+
+	dimensions = dimensions || {gridWidth: 1, gridHeight: 1};
+
+	return new cosmos.blocks.constructors[type](dimensions);
+};
+
 if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = Element; }
