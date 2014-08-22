0f0c744b248759f40882dade890021a87f9b25af
Daniel Chiu <daniechiu@gmail.com>
2014-08-22 15:31:27 -0700

    Move client-specific deployment grunt scripts back to client directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,6 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		compass: {
-			dist: {
-				options: {
-					sassDir: 'client/sass',
-					cssDir: 'client/public/stylesheets'
-				}
-			}
-		},
-		dust: {
-			dist: {
-				files: {
-					'client/public/js/templates.dust.js': 'client/views/**/*.dust'
-				},
-				options: {
-					wrapper: false,
-					runtime: false,
-					basePath: 'client/views'
-				}
-			}
-		},
 		karma: {
 			unit: {
 				options: {
@@ -62,29 +42,11 @@ module.exports = function(grunt) {
 			test: {
 				src: ['cosmos/tests/game.js']
 			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['compass', 'dust']
-			},
-			dust: {
-				files: 'client/views/**/*.dust',
-				tasks: ['dust']
-			},
-			options: {
-				atBegin: true
-			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-dust');
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-exec');
 	grunt.loadNpmTasks('grunt-jsbeautifier');
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('deploy', ['dust']);
 	grunt.registerTask('test', 'Runs tests for Cosmos.', function() {
 		if (grunt.option('no-compile') || grunt.option('nc')) {
 			grunt.task.run(['karma:unit']);
@@ -97,4 +59,5 @@ module.exports = function(grunt) {
 			]);
 		}
 	});
+	grunt.registerTask('default', ['test']);
 };
