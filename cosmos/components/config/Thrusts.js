5b10a1e93f7bf4e922c5e3c23e61d3f9d15e4fd3
Rafael Cosman <rafaelCosman@gmail.com>
2014-08-27 06:05:33 -0700

    Improves controls by increasing linear and angular damping

--- cosmos/components/config/Thrusts.js
+++ cosmos/components/config/Thrusts.js
@@ -1,6 +1,6 @@
 var Thrusts = {};
 
-var BASE_ENGINE_POWER = 0.005;
+var BASE_ENGINE_POWER = 0.01;
 Thrusts[IronEngineBlock.prototype.classId()] =
 {
 	value: 1 * BASE_ENGINE_POWER
@@ -16,7 +16,7 @@ Thrusts[DragonBreathEngineBlock.prototype.classId()] =
 	value: 3 * BASE_ENGINE_POWER
 };
 
-var BASE_THRUST_POWER = 0.001;
+var BASE_THRUST_POWER = 0.005;
 Thrusts[IronThrusterBlock.prototype.classId()] =
 {
 	value: 1 * BASE_THRUST_POWER
