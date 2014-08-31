7aec391da9712f784af416a295531705ce67aaca
Daniel Chiu <daniechiu@gmail.com>
2014-08-31 15:56:48 -0700

    Fix bug where server would crash if the client somehow managed to send a message requesting to construct a block at an occupied location.

--- cosmos/entities/BlockGrid.js
+++ cosmos/entities/BlockGrid.js
@@ -333,7 +333,11 @@ var BlockGrid = IgeEntity.extend({
 				// contiguous structure.
 				if (this.hasNeighbors(location)) {
 					var block = Block.fromType(data.selectedType);
-					self.put(block, new IgePoint2d(data.col, data.row), false);
+					// If put returns null, it means there was already a block at the requested
+					// location.
+					if (self.put(block, new IgePoint2d(data.col, data.row), false) === null) {
+						return false;
+					}
 					self.actions().push({
 						action: "put",
 						block: block.toJSON()
