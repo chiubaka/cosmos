45e65927747518a521c7ae5151e9e10e0f149ecd
Eric Thong <ethong@stanford.edu>
2014-09-04 18:35:07 -0700

    Refactor gameserver deploy. Add preview and dev configurations to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,10 +32,21 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		exec: {
 			ige_deploy: {
 				cmd: 'cd ige; node server/ige.js --deploy ../cosmos --to ../cosmos/tests ' +
 					'--clear true --clearClasses false'
+			}
+		},
+
+		production: {
+			// Arguments to server/ige.js to load cosmos
+			dev: {
+				scriptArgs: ['--dev' , '-g', 'cosmos']
+			},
+			preview: {
+				scriptArgs: ['--preview' , '-g', 'cosmos']
 			}
 		},
 
@@ -48,6 +59,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-exec');
 	grunt.loadNpmTasks('grunt-jsbeautifier');
+
 	grunt.registerTask('test', 'Runs tests for Cosmos.', function() {
 		if (grunt.option('no-compile') || grunt.option('nc')) {
 			grunt.task.run(['karma:unit']);
@@ -60,15 +72,19 @@ module.exports = function(grunt) {
 			]);
 		}
 	});
-	grunt.registerTask('preview', 'Runs the Cosmos game server as preview',
-		function() {
+
+	grunt.registerMultiTask('production', 'Runs the Cosmos game server in' +
+		'production (either dev or preview)', function() {
+		// call done() when async stuff is done so Grunt can move on
 		var done = this.async();
+		var self = this;
 		var pm2 = require('pm2');
 		pm2.connect(function(err) {
+			console.dir(self);
 			var gameServerPath = 'ige/server/ige.js';
 			var gameServerOpts = {
 				name: 'cosmos_game_server',
-				scriptArgs: ['--preview', '-g', 'cosmos'],
+				scriptArgs: [self.data.scriptArgs, '-g', 'cosmos'],
 				error: 'logs/error.log',
 				output: 'logs/output.log'
 			};
